d951c1185b1034a4c23b5081a7b0ae02de1b74ad
JhonnatanHHM-lambda <jonnathan.henao@lambdaanalytics.co>
2025-12-15 07:39:42 -0500

    feat(deploy): cambios para despliegue en vercel

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/LAMBDA_edupro_360_UI/',
+  base: '/',  // Cambia esto (o elimínalo, ya que '/' es el default)
   build: {
     outDir: 'dist'
   }
